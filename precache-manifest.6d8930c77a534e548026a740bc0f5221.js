808ca08d2924302f5e38d95dab18e2163a7cf3a0
Shilza <andgalatinov@gmail.com>
2019-03-10 14:48:44 +0300

    Updates

---
+++ precache-manifest.6d8930c77a534e548026a740bc0f5221.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d87ded07d52b01c3f63d",
+    "url": "/react-pretty-drawer/static/js/runtime~main.75ccef57.js"
+  },
+  {
+    "revision": "7cc68b3328279d394f7d",
+    "url": "/react-pretty-drawer/static/js/main.96790ad0.chunk.js"
+  },
+  {
+    "revision": "c6e7d9fd88a13a9418d7",
+    "url": "/react-pretty-drawer/static/js/2.a00be73d.chunk.js"
+  },
+  {
+    "revision": "7cc68b3328279d394f7d",
+    "url": "/react-pretty-drawer/static/css/main.2bdcfa70.chunk.css"
+  },
+  {
+    "revision": "896c80eb61a4d245fd3abfc0b7b725de",
+    "url": "/react-pretty-drawer/index.html"
+  }
+];
