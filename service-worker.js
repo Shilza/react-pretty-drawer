87bc9e468d778887f05b10fdd90f9fddd85e361b
Shilza <andgalatinov@gmail.com>
2019-03-17 14:59:40 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-pretty-drawer/precache-manifest.c1ef83b68f401832f87d286981fb854d.js"
+  "/react-pretty-drawer/precache-manifest.a57a739dab0b7470ea5a1d7a7f164000.js"
 );
 
 workbox.clientsClaim();
