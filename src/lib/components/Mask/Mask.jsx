26e86520098645c6431eb324af812b79a7873535
Shilza <andgalatinov@gmail.com>
2019-03-17 14:30:15 +0300

    Mask test-id && onClose function has been added

--- src/lib/components/Mask/Mask.jsx
+++ src/lib/components/Mask/Mask.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './mask.module.css';
 
-const Mask = ({ style }) => (
-    <div id="mask" className={styles.mask} style={style} />
+const Mask = ({ style, onClose }) => (
+    <div id="mask" data-testid="mask" className={styles.mask} style={style} onClick={onClose} />
 );
 
 export default React.memo(Mask);
