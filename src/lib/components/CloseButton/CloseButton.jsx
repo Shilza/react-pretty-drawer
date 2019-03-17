2364829f5620005a90072a4dfbbcfe90fcc75cc4
Shilza <andgalatinov@gmail.com>
2019-03-17 14:29:10 +0300

    Close button test-id has been added

--- src/lib/components/CloseButton/CloseButton.jsx
+++ src/lib/components/CloseButton/CloseButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './closeButton.module.css';
 
 const CloseButton = ({ onClose }) => (
-    <div role="button" className={styles.closeIcon} onClick={onClose} />
+    <div data-testid="closeButton" role="button" className={styles.closeIcon} onClick={onClose} />
 );
 
 export default React.memo(CloseButton);
